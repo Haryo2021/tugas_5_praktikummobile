7b060b4fdef83fc986dc4064fc3a8d6a4ece7d11
Haryo Bimantoro <102338707+Haryo2021@users.noreply.github.com>
2023-11-19 23:02:23 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'experiment_2_angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
